3416249cd9f59fd8576746326abc40c727dadddb
VICTORxEVO <yassirsbai04@gmail.com>
2024-07-22 13:16:42 +0100

    Final V4.0.1

--- philo/source/utils/philo_needs.c
+++ philo/source/utils/philo_needs.c
@@ -3,7 +3,7 @@
 bool    special_philo(t_philo *philo)
 {
     if (philo->ccu->n_philo == 1)
-        return (printt(philo, 'L'), usleep_v2(philo->ccu->t_death * 1000 + 100), true);
+        return (printt(philo, 'L'), usleep_v2(philo->ccu->t_death + 100), true);
     else if (philo->id % 2 == 0)
         usleep_v2(2);
     return (false);
